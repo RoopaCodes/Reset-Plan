d6e7b3c13406ddd1d8d1a56ceeb10382ae04e7cc
RoopaCodes <roopasingh2002@gmail.com>
2026-09-21 01:30:28 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this number whenever you upload a new version of the app.
-const CACHE = 'life-plan-v1';
+const CACHE = 'life-plan-v5';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
